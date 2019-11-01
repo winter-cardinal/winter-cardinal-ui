c5b241701643bf584d5372a993a66b119f6e69ee
Masashi Kojo <masashi.kojo@toshiba.co.jp>
2019-11-01 14:47:02 +0900

    Fix UtilFileDownloader#downloadUrl on iOS devices

--- src/main/typescript/wcardinal/ui/util/util-file-downloader.ts
+++ src/main/typescript/wcardinal/ui/util/util-file-downloader.ts
@@ -6,14 +6,20 @@
 export class UtilFileDownloader {
 	static downloadUrl( filename: string, url: string ): void {
 		const a = document.createElement( "a" );
-		a.href = url;
-		a.download = filename;
-		a.style.display = "none";
-		document.body.appendChild( a );
-		a.click();
-		setTimeout((): void => {
-			document.body.removeChild( a );
-		}, 100);
+		if( "download" in a ) {
+			a.href = url;
+			a.setAttribute( "download", filename );
+			a.style.display = "none";
+			document.body.appendChild( a );
+			a.click();
+			setTimeout((): void => {
+				document.body.removeChild( a );
+			}, 66);
+		} else {
+			if( ! window.open( url ) ) {
+				location.href = url;
+			}
+		}
 	}
 
 	static download( filename: string, contents: string ): void {
