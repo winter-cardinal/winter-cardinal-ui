c83129ca5e9003068a8e530cad881de7a511b50d
Do Huu Chien <chien.dohuu@toshiba-tsdv.com>
2019-12-26 12:08:23 +0700

    Add copyright, Fix bug of return in getInstance()

--- src/main/typescript/wcardinal/ui/theme/d-theme-default-configurations.ts
+++ src/main/typescript/wcardinal/ui/theme/d-theme-default-configurations.ts
@@ -1,8 +1,13 @@
+/*
+ * Copyright (C) 2019 Toshiba Corporation
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 import { DThemeDefaultConfiguration } from "./d-theme-default-configuration";
 import { DThemeDefaultConfigurationWhite } from "./d-theme-default-configuration-white";
 
 export class DThemeDefaultConfigurations {
-	private static DEFAULT_CLASS: (new () => DThemeDefaultConfiguration) | null = null;
+	private static DEFAULT_CLASS: ( new () => DThemeDefaultConfiguration ) | null = null;
 	private static INSTANCE: DThemeDefaultConfiguration;
 
 	/**
@@ -11,8 +16,8 @@ export class DThemeDefaultConfigurations {
 	 */
 	static getInstance(): DThemeDefaultConfiguration {
 		let result = this.INSTANCE;
-		if(result == null) {
-			result = new ( this.DEFAULT_CLASS || DThemeDefaultConfigurationWhite)();
+		if( result == null ) {
+			result = new ( this.DEFAULT_CLASS || DThemeDefaultConfigurationWhite )();
 			this.INSTANCE = result;
 		}
 		return result;
@@ -22,8 +27,7 @@ export class DThemeDefaultConfigurations {
 	 * Set default class for theme configuration.
 	 * @param defaultClass theme configuration.
 	 */
-	static setDefaultClass(defaultClass: (new () => DThemeDefaultConfiguration)): void {
+	static setDefaultClass( defaultClass: ( new () => DThemeDefaultConfiguration ) ): void {
 		this.DEFAULT_CLASS = defaultClass;
-		this.INSTANCE = new ( this.DEFAULT_CLASS || DThemeDefaultConfigurationWhite)();
 	}
 }
