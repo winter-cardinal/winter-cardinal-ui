712e1d75b98f398f8b6079a6dd83bc54f29c43fc
Nguyen Viet Nam <nam.nguyenvietiist@toshiba-tsdv.com>
2020-01-10 10:30:19 +0700

    Implement Dark Theme for html element, image, text
    - Create Dark Theme file
    - Create load theme method
    - Create html example for html element widget
    
    Change-Id: I184ca0abbc7e8e0004dc0a8fd23e4775dfa6c7b1

---
+++ src/main/typescript/wcardinal/ui/theme/dark/d-theme-dark-image.ts
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2019 Toshiba Corporation
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { DThemeImage } from "../../d-image";
+import { DThemeDarkImageBase } from "./d-theme-dark-image-base";
+
+export class DThemeDarkImage extends DThemeDarkImageBase implements DThemeImage {
+
+}
