4483339b9233d5cae31ee8a32e54eceb7f2e47ec
Nguyen Viet Nam <nam.nguyenvietiist@toshiba-tsdv.com>
2020-01-13 17:09:41 +0700

    Implement Dark theme for table body widgets
    - Create Dark theme class
    - Add to load dark theme method
    
    Change-Id: I900a1458b3c167a27f7eba3d7dadc14792e4104f

---
+++ src/main/typescript/wcardinal/ui/theme/dark/d-theme-dark-table-body.ts
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2019 Toshiba Corporation
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { DBaseInteractive } from "../../d-base-interactive";
+import { DBaseState } from "../../d-base-state";
+import { DCoordinateSize } from "../../d-coordinate";
+import { DCornerMask } from "../../d-corner";
+import { DThemeTableBody } from "../../d-table-body";
+import { DThemeDarkBase } from "./d-theme-dark-base";
+
+export class DThemeDarkTableBody extends DThemeDarkBase implements DThemeTableBody {
+	getWidth(): DCoordinateSize {
+		return "100%";
+	}
+
+	getCornerMask(): DCornerMask {
+		return DCornerMask.TOP;
+	}
+
+	getBorderColor( state: DBaseState ): number | null {
+		return null;
+	}
+
+	getRowHeight(): number {
+		return 30;
+	}
+
+	getInteractive(): DBaseInteractive {
+		return DBaseInteractive.BOTH;
+	}
+}
