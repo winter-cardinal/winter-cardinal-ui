33978e3b2db4c6c974f2a7cedb92415b89b54322
Nguyen Viet Nam <nam.nguyenvietiist@toshiba-tsdv.com>
2020-01-13 16:51:18 +0700

    Implement Dark theme for table body cell widgets
    - Create Dark theme class
    - Add to load dark theme method
    
    Change-Id: I1171e7641231d2ca5ec3e14478a30beec96d0d23

---
+++ src/main/typescript/wcardinal/ui/theme/dark/d-theme-dark-table-body-cell-color.ts
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2019 Toshiba Corporation
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { DBaseState } from "../../d-base-state";
+import { DBorderMask } from "../../d-border";
+import { DCoordinateSize } from "../../d-coordinate";
+import { DCornerMask } from "../../d-corner";
+import { DThemeTableBodyCellColor } from "../../d-table-body-cell-color";
+import { DThemeDarkButtonColor } from "./d-theme-dark-button-color";
+import { DThemeDarkTableBodyCells } from "./d-theme-dark-table-body-cells";
+
+export class DThemeDarkTableBodyCellColor extends DThemeDarkButtonColor implements DThemeTableBodyCellColor {
+	getBackgroundColor( state: DBaseState ): number | null {
+		return DThemeDarkTableBodyCells.getBackgroundColor( state );
+	}
+
+	getBackgroundAlpha( state: DBaseState ): number {
+		return DThemeDarkTableBodyCells.getBackgroundAlpha( state );
+	}
+
+	getBorderColor( state: DBaseState ): number | null {
+		return DThemeDarkTableBodyCells.getBorderColor( state );
+	}
+
+	getBorderAlign( state: DBaseState ): number {
+		return DThemeDarkTableBodyCells.getBorderAlign( state );
+	}
+
+	getBorderMask( state: DBaseState ): DBorderMask {
+		return DThemeDarkTableBodyCells.getBorderMask( state );
+	}
+
+	getColor( state: DBaseState ): number {
+		return DThemeDarkTableBodyCells.getColor( state );
+	}
+
+	getAlpha( state: DBaseState ): number {
+		return DThemeDarkTableBodyCells.getAlpha( state );
+	}
+
+	getHeight(): DCoordinateSize {
+		return DThemeDarkTableBodyCells.getHeight();
+	}
+
+	getCornerMask(): DCornerMask {
+		return DThemeDarkTableBodyCells.getCornerMask();
+	}
+}
