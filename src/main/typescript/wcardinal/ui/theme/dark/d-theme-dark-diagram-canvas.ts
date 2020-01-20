68c22659ba51f64ddd4a3ed5d87b42f80026e02b
Do Huu Chien <chiendh1705@gmail.com>
2020-01-20 17:03:06 +0700

    Implement dark themes for diagram and diagram editor (#22)

---
+++ src/main/typescript/wcardinal/ui/theme/dark/d-theme-dark-diagram-canvas.ts
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2019 Toshiba Corporation
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { DThemeDiagramCanvas } from "../../d-diagram-canvas";
+import { DThemeDarkDiagramCanvasBase } from "./d-theme-dark-diagram-canvas-base";
+
+export class DThemeDarkDiagramCanvas extends DThemeDarkDiagramCanvasBase implements DThemeDiagramCanvas {
+
+}
---
+++ src/main/typescript/wcardinal/ui/theme/dark/d-theme-dark-diagram.ts
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2019 Toshiba Corporation
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { DThemeDiagram } from "../../d-diagram";
+import { DThemeDarkDiagramBase } from "./d-theme-dark-diagram-base";
+
+export class DThemeDarkDiagram extends DThemeDarkDiagramBase implements DThemeDiagram {
+
+}
