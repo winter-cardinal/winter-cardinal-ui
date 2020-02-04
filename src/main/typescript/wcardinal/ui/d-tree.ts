76e21d4280a3aba86a5a73fcac526e0aa2282450
Vu Le Quang <vu.lequang@toshiba-tsdv.com>
2020-02-04 18:25:24 +0700

    Implement toggle tree API
    - create onToggle() function to handle toggle action
    
    [refs #2077, #2083]
    
    Change-Id: I0e6291f89a838f244fac5e7c30e84b514a7e8694

--- src/main/typescript/wcardinal/ui/d-tree.ts
+++ src/main/typescript/wcardinal/ui/d-tree.ts
@@ -97,6 +97,9 @@ export class DTree <
 			if(items.length < itemOptionsShown.length) {
 				for (let i = items.length; i < itemOptionsShown.length; i++) {
 					const treeItem = new DTreeItem(itemOptionsShown[i])
+					treeItem.on(UtilPointerEvent.down, (): void => {
+						this.onToggle(itemOptionsShown[i].itemPosition)
+					});
 					content.addChild(treeItem)
 				}
 			} else if(items.length > itemOptionsShown.length) {
@@ -109,21 +112,46 @@ export class DTree <
 				items[i] = items[i].updateItem(itemOptionsShown[i])
 			}
 		}
+		private onToggle(itemPosition: number[]) {
+			if(this._value instanceof Array) {
+				let items = this._value
 
+				//access to data of selected item in this.value to update expanded attribute
+				for (let i = 0; i < itemPosition.length - 1; i++) {
+					items = items[itemPosition[i]].items
+				}
+				const item = items[itemPosition[itemPosition.length - 1]]
+				item.expanded = !item.expanded
+
+				//reset data of tree widget
+				this._itemOptions = {}
+				this._itemOptionsShowable.length = 0
+				this._itemY = 0
+
+				//re-render tree
+				this.mapped([], this._value)
+				this.update(true)
+			}
+		}
 		private mapped(parentPosition: number[], items: VALUE | null) {
 			if(items instanceof Array ) {
 				for (let i = 0; i < items.length; i++) {
 					const item = items[i];
 					if (item) {
+						//position of item in this._value
 						const itemPosition: number[] = parentPosition.concat([i])
 
+						//calculate padding value of item
 						let paddingValue: number = itemPosition.length * 25
 						const isParent: boolean = item.items && (item.items.length > 0)
 						paddingValue = isParent ? paddingValue - 20 : paddingValue
 						const itemName = item.name ? item.name : null
+
+						//set default expand status of item is false
 						if(!item.expanded) {
-							item.expanded = true
+							item.expanded = false
 						}
+
 						let itemOptions = {
 							itemPosition: itemPosition,
 							bus: this._bus,
@@ -137,7 +165,13 @@ export class DTree <
 								left: paddingValue
 							}
 						}
+
 						const parentItemOptions = this._itemOptions[parentPosition.join('-')]
+
+						/* displayed items need to satisfy 1 of the 2 conditions:
+						1. is root item
+						2. the parent item is show and expand
+						*/
 						if(parentPosition.length == 0
 							|| (parentItemOptions
 								&& parentItemOptions.expanded
