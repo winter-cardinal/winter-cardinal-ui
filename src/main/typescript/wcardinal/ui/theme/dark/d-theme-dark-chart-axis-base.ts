0b1c385a83420f3d891455560946ead08fc4d311
Nguyen Viet Nam <nam.nguyenvietiist@toshiba-tsdv.com>
2020-02-05 15:54:07 +0700

    Update Stroke Color chart for Dark theme
    Change-Id: I69feb936ccc7aad966fc77c323aa908b988329b4

--- src/main/typescript/wcardinal/ui/theme/dark/d-theme-dark-chart-axis-base.ts
+++ src/main/typescript/wcardinal/ui/theme/dark/d-theme-dark-chart-axis-base.ts
@@ -73,7 +73,7 @@ export class DThemeDarkChartAxisBase implements DThemeChartAxisBase {
 	}
 
 	getStrokeColor(): number {
-		return EShapeDefaults.STROKE_COLOR_DARK;
+		return 0xdedede;
 	}
 
 	getStrokeAlpha(): number {
