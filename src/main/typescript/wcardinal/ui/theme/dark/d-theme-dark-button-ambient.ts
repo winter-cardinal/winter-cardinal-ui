5db21db7853735a09f18f944089fee5b61878648
Vu Le Quang <vu.lequang@toshiba-tsdv.com>
2020-03-24 10:53:28 +0700

    Fix dark theme defect for button widgets
    Change-Id: I74e2e8c09fa7523d4404dda11dcb8bd1242affc1

--- src/main/typescript/wcardinal/ui/theme/dark/d-theme-dark-button-ambient.ts
+++ src/main/typescript/wcardinal/ui/theme/dark/d-theme-dark-button-ambient.ts
@@ -6,35 +6,26 @@
 import { DBaseState } from "../../d-base-state";
 import { DBaseStates } from "../../d-base-states";
 import { DThemeButtonAmbient } from "../../d-button-ambient";
+import { UtilRgb } from "../../util/util-rgb";
 import { DThemeDarkButtonBase } from "./d-theme-dark-button-base";
 import { DThemeDarkConstants } from "./d-theme-dark-constants";
 
 export class DThemeDarkButtonAmbient extends DThemeDarkButtonBase implements DThemeButtonAmbient {
-	COLOR = 0x646464;
+	COLOR = 0x131313;
 
 	getBackgroundColor( state: DBaseState ): number | null {
+		if( DBaseStates.isDisabled( state ) ) {
+			return UtilRgb.blend( 0x000000, 0xFFFFFF, DThemeDarkConstants.DISABLED_ALPHA );
+		}
 		if( DBaseStates.isActive( state ) ) {
-			return DThemeDarkConstants.HIGHLIGHT_COLOR;
+			return UtilRgb.blend( this.COLOR, this.getColor( DBaseState.ACTIVE ), DThemeDarkConstants.ACTIVE_ALPHA );
 		}
-		return this.COLOR;
-	}
-
-	getBackgroundAlpha( state: DBaseState ): number {
-		if( ! DBaseStates.isDisabled( state ) ) {
-			if( DBaseStates.isActive( state ) ) {
-				return 1.0;
-			}
-			if( DBaseStates.isPressed( state ) ) {
-				return DThemeDarkConstants.WEAK_HIGHLIGHT_ALPHA * 2;
-			}
-			if( DBaseStates.isFocused( state ) || DBaseStates.isHovered( state ) ) {
-				return DThemeDarkConstants.WEAK_HIGHLIGHT_ALPHA;
-			}
+		if( DBaseStates.isPressed( state ) ) {
+			return UtilRgb.blend( this.COLOR, this.getColor( DBaseState.PRESSED ), DThemeDarkConstants.PRESSED_ALPHA );
 		}
-		return 0;
-	}
-
-	getBorderColor( state: DBaseState ): number | null {
+		if( DBaseStates.isFocused( state ) || DBaseStates.isHovered( state ) ) {
+			return UtilRgb.blend( this.COLOR, this.getColor( DBaseState.HOVERED ), DThemeDarkConstants.FOCUSED_ALPHA );
+		}
 		return null;
 	}
 }
--- src/main/typescript/wcardinal/ui/theme/dark/d-theme-dark-button-secondary.ts
+++ src/main/typescript/wcardinal/ui/theme/dark/d-theme-dark-button-secondary.ts
@@ -11,33 +11,26 @@ import { DThemeDarkConstants } from "./d-theme-dark-constants";
 
 export class DThemeDarkButtonSecondary extends DThemeDarkButtonBase {
 	COLOR = 0xB894F6;
-	COLOR_HOVERED = UtilRgb.darken( this.COLOR, DThemeDarkConstants.FOCUSED_ALPHA );
-	COLOR_PRESSED = UtilRgb.darken( this.COLOR, DThemeDarkConstants.PRESSED_ALPHA );
-	COLOR_DISABLED = UtilRgb.brighten( 0x000000, DThemeDarkConstants.DISABLED_ALPHA );
 
 	getBackgroundColor( state: DBaseState ): number | null {
 		if( DBaseStates.isDisabled( state ) ) {
-			return this.COLOR_DISABLED;
+			return UtilRgb.blend( 0x000000, 0xFFFFFF, DThemeDarkConstants.DISABLED_ALPHA );
 		}
-		if( DBaseStates.isPressed( state ) || DBaseStates.isActive( state ) ) {
-			return this.COLOR_PRESSED;
+		if( DBaseStates.isActive( state ) ) {
+			return UtilRgb.blend( this.COLOR, this.getColor( DBaseState.ACTIVE ), DThemeDarkConstants.ACTIVE_ALPHA );
+		}
+		if( DBaseStates.isPressed( state ) ) {
+			return UtilRgb.blend( this.COLOR, this.getColor( DBaseState.PRESSED ), DThemeDarkConstants.PRESSED_ALPHA );
 		}
 		if( DBaseStates.isFocused( state ) || DBaseStates.isHovered( state ) ) {
-			return this.COLOR_HOVERED;
+			return UtilRgb.blend( this.COLOR, this.getColor( DBaseState.HOVERED ), DThemeDarkConstants.FOCUSED_ALPHA );
 		}
 		return this.COLOR;
 	}
 
-	getBorderColor( state: DBaseState ): number | null {
-		if( DBaseStates.isDisabled( state ) ) {
-			return this.COLOR_DISABLED;
-		}
-		return null;
-	}
-
 	getColor( state: DBaseState ): number {
-		if(DBaseStates.isDisabled( state ) ) {
-			return super.getColor( state );
+		if( DBaseStates.isDisabled( state ) ) {
+			return UtilRgb.blend( 0x000000, 0xFFFFFF, DThemeDarkConstants.DISABLED_TEXT_ALPHA );
 		}
 		return 0x000000;
 	}
