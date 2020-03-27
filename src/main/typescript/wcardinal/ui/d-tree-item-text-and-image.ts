5f2d29adf88e3c1ab1d46443553f8a2ce03fc8a9
Mofnire <masashi.kojo@toshiba.co.jp>
2020-03-27 09:53:39 +0900

    Update d-tree-item-text-and-image.ts

--- src/main/typescript/wcardinal/ui/d-tree-item-text-and-image.ts
+++ src/main/typescript/wcardinal/ui/d-tree-item-text-and-image.ts
@@ -5,19 +5,20 @@
 import { DImage, DImageOptions, DThemeImage } from "./d-image";
 
 export interface DTreeItemTextAndImageOptions<
-	THEME extends DThemeTreeItemTextAndImage = DThemeTreeItemTextAndImage>
-	extends DImageOptions <string, THEME> {}
+	THEME extends DThemeTreeItemTextAndImage = DThemeTreeItemTextAndImage
+> extends DImageOptions <string, THEME> {
+
+}
 
 export interface DThemeTreeItemTextAndImage extends DThemeImage {
+
 }
 
 export class DTreeItemTextAndImage<
 	THEME extends DThemeTreeItemTextAndImage = DThemeTreeItemTextAndImage,
-	OPTIONS extends DTreeItemTextAndImageOptions <THEME> = DTreeItemTextAndImageOptions <THEME>
-	>
-	extends DImage <string, THEME, OPTIONS> {
-
-		protected getType(): string {
-			return "DTreeItemTextAndImage";
-		}
+	OPTIONS extends DTreeItemTextAndImageOptions<THEME> = DTreeItemTextAndImageOptions<THEME>
+> extends DImage<string, THEME, OPTIONS> {
+	protected getType(): string {
+		return "DTreeItemTextAndImage";
 	}
+}
