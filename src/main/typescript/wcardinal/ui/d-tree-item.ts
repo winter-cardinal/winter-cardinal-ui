8f2a89bd9052c8cba3d32dc9d919981ae4f9b20f
Mofnire <masashi.kojo@toshiba.co.jp>
2020-03-27 09:57:24 +0900

    Update d-tree-item.ts

--- src/main/typescript/wcardinal/ui/d-tree-item.ts
+++ src/main/typescript/wcardinal/ui/d-tree-item.ts
@@ -11,17 +11,17 @@ import { DTreeItemToggleIcon} from "./d-tree-item-toggle-icon";
 import { UtilPointerEvent } from "./util";
 
 export interface DTreeItemOptions<
-	THEME extends DThemeTreeItem = DThemeTreeItem>
-	extends DLayoutHorizontalOptions <THEME> {
-		text: string;
-		isParent: boolean;
-		expanded: boolean;
-		rawData: DTreeItemRawData;
-		y: number;
-		level: number;
-		showable?: boolean;
-		image?: DisplayObject | Texture | null;
-	}
+	THEME extends DThemeTreeItem = DThemeTreeItem
+> extends DLayoutHorizontalOptions <THEME> {
+	text: string;
+	isParent: boolean;
+	expanded: boolean;
+	rawData: DTreeItemRawData;
+	y: number;
+	level: number;
+	showable?: boolean;
+	image?: DisplayObject | Texture | null;
+}
 
 export interface DThemeTreeItem extends DThemeLayoutHorizontal {
 	getPaddingByLevel( level: number ): number;
@@ -51,8 +51,7 @@ const toYCoordinate = ( options?: DTreeItemOptions ): number => {
 };
 
 const toRawData = ( options?: DTreeItemOptions ): DTreeItemRawData => {
-	return options ? options.rawData
-	: {
+	return options ? options.rawData : {
 		text: "",
 		children: []
 	};
@@ -61,104 +60,103 @@ const toRawData = ( options?: DTreeItemOptions ): DTreeItemRawData => {
 const toPaddingLeft = <THEME extends DThemeTreeItem>(
 	theme: DThemeTreeItem,
 	options?: DTreeItemOptions<THEME>
-	): number => {
+): number => {
 	const level = options ? options.level : 0;
 	return theme.getPaddingByLevel( level );
 };
 
 const toTreeItemTextAndImage = ( options?: DTreeItemOptions ): DTreeItemTextAndImage => {
-	return new DTreeItemTextAndImage( {
+	return new DTreeItemTextAndImage({
 		text: {
 			value: toText( options )
 		},
 		image: {
 			source: toImage( options )
 		}
-	} );
+	});
 };
 
 export class DTreeItem<
 	THEME extends DThemeTreeItem = DThemeTreeItem,
 	OPTIONS extends DTreeItemOptions <THEME> = DTreeItemOptions <THEME>
-	>
-	extends DLayoutHorizontal < THEME, OPTIONS > {
-		protected _isParent!: boolean;
-		protected _isExpanded!: boolean;
-		protected _rawData!: DTreeItemRawData;
-		protected _textAndImage!: DTreeItemTextAndImage;
-		protected _icon!: DTreeItemToggleIcon;
+> extends DLayoutHorizontal < THEME, OPTIONS > {
+	protected _isParent!: boolean;
+	protected _isExpanded!: boolean;
+	protected _rawData!: DTreeItemRawData;
+	protected _textAndImage!: DTreeItemTextAndImage;
+	protected _icon!: DTreeItemToggleIcon;
 
-		protected init( options ?: OPTIONS ) {
-			super.init( options );
-			// get isParent option
-			this._isParent = isParent( options );
-			// get isExpand option
-			this._isExpanded = isExpanded( options );
-			// get raw data
-			this._rawData = toRawData( options );
+	protected init( options ?: OPTIONS ) {
+		super.init( options );
+		// get isParent option
+		this._isParent = isParent( options );
+		// get isExpand option
+		this._isExpanded = isExpanded( options );
+		// get raw data
+		this._rawData = toRawData( options );
 
-			this._icon = new DTreeItemToggleIcon();
-			this._textAndImage = toTreeItemTextAndImage( options );
+		this._icon = new DTreeItemToggleIcon();
+		this._textAndImage = toTreeItemTextAndImage( options );
 
-			this.addChild( this._icon );
-			this.addChild( this._textAndImage );
+		this.addChild( this._icon );
+		this.addChild( this._textAndImage );
 
-			this._icon.on( UtilPointerEvent.down, (): void => {
-				this.onToggle();
-			} );
-			this._textAndImage.on( UtilPointerEvent.down, ( e: interaction.InteractionEvent ): void => {
-				this.onSelect( e );
-			} );
+		this._icon.on( UtilPointerEvent.down, (): void => {
+			this.onToggle();
+		});
+		this._textAndImage.on( UtilPointerEvent.down, ( e: interaction.InteractionEvent ): void => {
+			this.onSelect( e );
+		});
 
-			// update states
-			this.updateStates( false );
-		}
-
-		protected onSelect( e: interaction.InteractionEvent ): void {
-			this.emit( "select", e );
-		}
-
-		public onToggle(): void {
-			this.emit( "toggle" );
-		}
-
-		public isExpanded(): boolean {
-			return this._isExpanded;
-		}
-
-		public isParent(): boolean {
-			return this._isParent;
-		}
-
-		public getRawData(): DTreeItemRawData {
-			return this._rawData;
-		}
-
-		public update( options: OPTIONS, isActive: boolean ) {
-			this._textAndImage.text = toText( options );
-			this._textAndImage.image = toImage( options );
-			this._rawData = toRawData( options );
-			this._padding.left = toPaddingLeft( this.theme, options );
-			this.position.y = toYCoordinate( options );
-			this._isParent = isParent( options );
-			this._isExpanded = isExpanded( options );
-			this.updateStates( isActive );
-			return this;
-		}
-
-		public updateActiveState( isActive: boolean ) {
-			this.setActive( isActive );
-			this._icon.setState( DTreeItemState.SELECTED, isActive );
-			this._textAndImage.setState( DTreeItemState.SELECTED, isActive );
-		}
-
-		protected updateStates( isActive: boolean ): void {
-			this.updateActiveState( isActive );
-			this._icon.setState( DTreeItemState.COLLAPSED, !this._isExpanded && this._isParent );
-			this._icon.setState( DTreeItemState.EXPANDED, !!this._isExpanded && this._isParent );
-		}
-
-		protected getType(): string {
-			return "DTreeItem";
-		}
+		// update states
+		this.updateStates( false );
 	}
+
+	protected onSelect( e: interaction.InteractionEvent ): void {
+		this.emit( "select", e );
+	}
+
+	public onToggle(): void {
+		this.emit( "toggle" );
+	}
+
+	public isExpanded(): boolean {
+		return this._isExpanded;
+	}
+
+	public isParent(): boolean {
+		return this._isParent;
+	}
+
+	public getRawData(): DTreeItemRawData {
+		return this._rawData;
+	}
+
+	public update( options: OPTIONS, isActive: boolean ) {
+		this._textAndImage.text = toText( options );
+		this._textAndImage.image = toImage( options );
+		this._rawData = toRawData( options );
+		this._padding.left = toPaddingLeft( this.theme, options );
+		this.position.y = toYCoordinate( options );
+		this._isParent = isParent( options );
+		this._isExpanded = isExpanded( options );
+		this.updateStates( isActive );
+		return this;
+	}
+
+	public updateActiveState( isActive: boolean ) {
+		this.setActive( isActive );
+		this._icon.setState( DTreeItemState.SELECTED, isActive );
+		this._textAndImage.setState( DTreeItemState.SELECTED, isActive );
+	}
+
+	protected updateStates( isActive: boolean ): void {
+		this.updateActiveState( isActive );
+		this._icon.setState( DTreeItemState.COLLAPSED, !this._isExpanded && this._isParent );
+		this._icon.setState( DTreeItemState.EXPANDED, !!this._isExpanded && this._isParent );
+	}
+
+	protected getType(): string {
+		return "DTreeItem";
+	}
+}
