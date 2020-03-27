85891dbeafdbff83c0e97a630b264b7815d1c29e
Mofnire <masashi.kojo@toshiba.co.jp>
2020-03-27 09:54:24 +0900

    Update d-tree-item-toggle-icon.ts

--- src/main/typescript/wcardinal/ui/d-tree-item-toggle-icon.ts
+++ src/main/typescript/wcardinal/ui/d-tree-item-toggle-icon.ts
@@ -5,18 +5,20 @@
 import { DImage, DImageOptions, DThemeImage } from "./d-image";
 
 export interface DTreeItemToggleIconOptions<
-	THEME extends DThemeTreeItemToggleIcon = DThemeTreeItemToggleIcon>
-	extends DImageOptions <string, THEME> {}
+	THEME extends DThemeTreeItemToggleIcon = DThemeTreeItemToggleIcon
+> extends DImageOptions<string, THEME> {
+
+}
 
 export interface DThemeTreeItemToggleIcon extends DThemeImage {
+
 }
 
 export class DTreeItemToggleIcon<
 	THEME extends DThemeTreeItemToggleIcon = DThemeTreeItemToggleIcon,
-	OPTIONS extends DTreeItemToggleIconOptions <THEME> = DTreeItemToggleIconOptions <THEME>
-	>
-	extends DImage <string, THEME, OPTIONS> {
-		protected getType(): string {
-			return "DTreeItemToggleIcon";
-		}
+	OPTIONS extends DTreeItemToggleIconOptions<THEME> = DTreeItemToggleIconOptions<THEME>
+> extends DImage <string, THEME, OPTIONS> {
+	protected getType(): string {
+		return "DTreeItemToggleIcon";
 	}
+}
