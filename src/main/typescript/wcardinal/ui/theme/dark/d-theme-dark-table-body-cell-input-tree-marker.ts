aac32bf5215a3e8326c25ebe6d27d5256fbee9b4
Masashi Kojo <masashi.kojo@toshiba.co.jp>
2021-01-26 12:22:14 +0900

    Fix for the DTable dark theme issue

--- src/main/typescript/wcardinal/ui/theme/dark/d-theme-dark-table-body-cell-input-tree-marker.ts
+++ src/main/typescript/wcardinal/ui/theme/dark/d-theme-dark-table-body-cell-input-tree-marker.ts
@@ -7,35 +7,67 @@ import { DisplayObject, Texture } from "pixi.js";
 import { DAlignHorizontal } from "../../d-align-horizontal";
 import { DAlignWith } from "../../d-align-with";
 import { DBaseStateSet } from "../../d-base-state-set";
-import { DCoordinateSize } from "../../d-coordinate";
+import { DBorderMask } from "../../d-border-mask";
+import { DCoordinatePosition, DCoordinateSize } from "../../d-coordinate";
+import { DCornerMask } from "../../d-corner-mask";
 import { DThemeTableBodyCellInputTreeMarker } from "../../d-table-body-cell-input-tree-marker";
 import { DThemeDarkButtonBase } from "./d-theme-dark-button-base";
 import { DThemeDarkTableBodyCellTrees } from "./d-theme-dark-table-body-cell-trees";
+import { DThemeDarkTableBodyCells } from "./d-theme-dark-table-body-cells";
 
 export class DThemeDarkTableBodyCellInputTreeMarker<VALUE = unknown> extends DThemeDarkButtonBase<VALUE>
 	implements DThemeTableBodyCellInputTreeMarker<VALUE> {
 
+	getX(): DCoordinatePosition {
+		return "padding";
+	}
+
+	getY(): DCoordinatePosition {
+		return "padding";
+	}
+
 	getHeight(): DCoordinateSize {
-		return "100%";
+		return DThemeDarkTableBodyCells.getHeight();
 	}
 
 	getBackgroundColor( state: DBaseStateSet ): number | null {
-		return null;
+		return DThemeDarkTableBodyCells.getBackgroundColor( state );
+	}
+
+	getBackgroundAlpha( state: DBaseStateSet ): number {
+		return DThemeDarkTableBodyCells.getBackgroundAlpha( state );
 	}
 
 	getBorderColor( state: DBaseStateSet ): number | null {
 		return null;
 	}
 
+	getBorderAlign( state: DBaseStateSet ): number {
+		return DThemeDarkTableBodyCells.getBorderAlign( state );
+	}
+
+	getBorderMask( state: DBaseStateSet ): DBorderMask {
+		return DThemeDarkTableBodyCells.getBorderMask( state );
+	}
+
+	getColor( state: DBaseStateSet ): number {
+		return DThemeDarkTableBodyCells.getColor( state );
+	}
+
+	getAlpha( state: DBaseStateSet ): number {
+		return DThemeDarkTableBodyCells.getAlpha( state );
+	}
+
+	getCornerMask(): DCornerMask {
+		return DThemeDarkTableBodyCells.getCornerMask();
+	}
+
 	getOutlineColor( state: DBaseStateSet ): number | null {
 		return null;
 	}
 
-	getCursor( state: DBaseStateSet ): string {
-		if( ! state.isActionable ) {
-			return "";
-		}
-		return "pointer";
+	getOutlineAlign( state: DBaseStateSet ): number {
+		return DThemeDarkTableBodyCells.getOutlineAlign( state );
 	}
 
 	getImageAlignWith(): DAlignWith {
--- src/main/typescript/wcardinal/ui/theme/dark/d-theme-dark-table-body-cell-input-tree.ts
+++ src/main/typescript/wcardinal/ui/theme/dark/d-theme-dark-table-body-cell-input-tree.ts
@@ -5,75 +5,34 @@
 
 import { DBaseInteractive } from "../../d-base-interactive";
 import { DBaseStateSet } from "../../d-base-state-set";
-import { DBorderMask } from "../../d-border-mask";
 import { DCoordinateSize } from "../../d-coordinate";
-import { DCornerMask } from "../../d-corner-mask";
 import { DThemeTableBodyCellInputTree } from "../../d-table-body-cell-input-tree";
-import { DThemeDarkLayoutHorizontal } from "./d-theme-dark-layout-horizontal";
+import { DThemeDarkBase } from "./d-theme-dark-base";
 import { DThemeDarkTableBodyCellTrees } from "./d-theme-dark-table-body-cell-trees";
 import { DThemeDarkTableBodyCells } from "./d-theme-dark-table-body-cells";
 
 DThemeDarkTableBodyCellTrees.init();
 
-export class DThemeDarkTableBodyCellInputTree extends DThemeDarkLayoutHorizontal
+export class DThemeDarkTableBodyCellInputTree extends DThemeDarkBase
 	implements DThemeTableBodyCellInputTree {
-
-	getLevelPadding( level: number ): number {
-		return 24 + level * 20;
-	}
-
-	getMargin(): number {
-		return 0;
-	}
-
-	getInteractive(): DBaseInteractive {
-		return DBaseInteractive.BOTH;
-	}
-
-	getBackgroundColor( state: DBaseStateSet ): number | null {
-		return DThemeDarkTableBodyCells.getBackgroundColor( state );
-	}
-
-	getBackgroundAlpha( state: DBaseStateSet ): number {
-		return DThemeDarkTableBodyCells.getBackgroundAlpha( state );
-	}
-
-	getBorderColor( state: DBaseStateSet ): number | null {
-		return DThemeDarkTableBodyCells.getBorderColor( state );
-	}
-
-	getBorderAlign( state: DBaseStateSet ): number {
-		return DThemeDarkTableBodyCells.getBorderAlign( state );
-	}
-
-	getBorderMask( state: DBaseStateSet ): DBorderMask {
-		return DThemeDarkTableBodyCells.getBorderMask( state );
-	}
-
-	getColor( state: DBaseStateSet ): number {
-		return DThemeDarkTableBodyCells.getColor( state );
-	}
-
-	getAlpha( state: DBaseStateSet ): number {
-		return DThemeDarkTableBodyCells.getAlpha( state );
-	}
 
 	getHeight(): DCoordinateSize {
 		return DThemeDarkTableBodyCells.getHeight();
 	}
 
-	getCornerMask(): DCornerMask {
-		return DThemeDarkTableBodyCells.getCornerMask();
+	getBackgroundColor( state: DBaseStateSet ): number | null {
+		return null;
 	}
 
-	getOutlineColor( state: DBaseStateSet ): number | null {
-		if( state.inDisabled ) {
-			return null;
-		}
-		return super.getOutlineColor( state );
+	getBorderColor( state: DBaseStateSet ): number | null {
+		return null;
 	}
 
-	getOutlineAlign( state: DBaseStateSet ): number {
-		return DThemeDarkTableBodyCells.getOutlineAlign( state );
+	getLevelPadding( level: number ): number {
+		return 24 + level * 20;
+	}
+
+	getInteractive(): DBaseInteractive {
+		return DBaseInteractive.CHILDREN;
 	}
 }
