cb111a2961618c5e310be517ee917a86c29c0dfa
Masashi Kojo <masashi.kojo@toshiba.co.jp>
2021-12-16 12:18:26 +0900

    fix: DNote textures of the dark theme

--- src/main/typescript/wcardinal/ui/theme/dark/d-theme-dark-note-error.ts
+++ src/main/typescript/wcardinal/ui/theme/dark/d-theme-dark-note-error.ts
@@ -11,7 +11,7 @@ import { DThemeDarkNote } from "./d-theme-dark-note";
 
 /* eslint-disable prettier/prettier */
 DThemeDarkAtlas.add("note_erorr", 48, 48,
-	`<g transform="translate(-3,3) scale(3,3)">` +
+	`<g transform="translate(-3,3) scale(2,2)">` +
 		'<path d="M12 5.99L19.53 19H4.47L12 5.99M12 2L1 21h22L12 2zm1 14h-2v2h2v-2zm0-6h-2v4h2v-4z" fill="#fff"/>' +
 	`</g>`
 );
