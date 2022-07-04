9114fa369021b7d017cf7113cec0fdb263258652
Masashi Kojo <masashi.kojo@toshiba.co.jp>
2022-07-04 21:02:41 +0900

    fix: wrong clip path issue

--- src/main/typescript/wcardinal/ui/util/util-html-element-clipper-ex-impl.ts
+++ src/main/typescript/wcardinal/ui/util/util-html-element-clipper-ex-impl.ts
@@ -7,12 +7,6 @@ import { Matrix, Rectangle } from "pixi.js";
 import { UtilHtmlElementClipperEx } from "./util-html-element-clipper-ex";
 import { UtilHtmlElementClipperExRects } from "./util-html-element-clipper-ex-rects";
 
-const SVG_XMLNS = "http://www.w3.org/2000/svg";
-
-const toS = (target: number): string => {
-	return 1 < target ? "1" : target < 0 ? "0" : target.toFixed(5);
-};
-
 export class UtilHtmlElementClipperExImpl implements UtilHtmlElementClipperEx {
 	private static WORK_MATRIX?: Matrix;
 	private static WORK_RECTS?: number[];
@@ -22,6 +16,7 @@ export class UtilHtmlElementClipperExImpl implements UtilHtmlElementClipperEx {
 	private _pathD: string;
 
 	constructor(container: HTMLElement) {
+		const SVG_XMLNS = "http://www.w3.org/2000/svg";
 		const element = document.createElementNS(SVG_XMLNS, "svg");
 		element.setAttribute("style", "position: absolute;");
 		element.setAttribute("width", "0px");
@@ -90,7 +85,11 @@ export class UtilHtmlElementClipperExImpl implements UtilHtmlElementClipperEx {
 		matrix.translate(-elementRect.x, -elementRect.y);
 		matrix.scale(1 / w, 1 / h);
 
-		let result = "M0,0 H1 V1 H0Z";
+		let xmin = -1;
+		let xmax = +2;
+		let ymin = -1;
+		let ymax = +2;
+		let result = "";
 		const a = matrix.a;
 		const b = matrix.b;
 		const c = matrix.c;
@@ -99,22 +98,39 @@ export class UtilHtmlElementClipperExImpl implements UtilHtmlElementClipperEx {
 		const ty = matrix.ty;
 		const rects = (UtilHtmlElementClipperExImpl.WORK_RECTS ??= []);
 		const rectsLength = this.toRects(clipperExRects, rects);
+		const precision = 5;
 		for (let i = 0; i < rectsLength; i += 4) {
-			const d0 = rects[i + 0];
-			const d1 = rects[i + 1];
-			const d2 = rects[i + 2];
-			const d3 = rects[i + 3];
-			const x0 = toS(a * d0 + c * d1 + tx);
-			const y0 = toS(b * d0 + d * d1 + ty);
-			const x1 = toS(a * d2 + c * d1 + tx);
-			const y1 = toS(b * d2 + d * d1 + ty);
-			const x2 = toS(a * d2 + c * d3 + tx);
-			const y2 = toS(b * d2 + d * d3 + ty);
-			const x3 = toS(a * d0 + c * d3 + tx);
-			const y3 = toS(b * d0 + d * d3 + ty);
-			result += ` M${x0},${y0} L${x1},${y1} L${x2},${y2} L${x3},${y3}Z`;
+			const x0 = rects[i + 0];
+			const y0 = rects[i + 1];
+			const x1 = rects[i + 2];
+			const y1 = rects[i + 3];
+
+			const x2 = a * x0 + c * y0 + tx;
+			const y2 = b * x0 + d * y0 + ty;
+			const x3 = a * x1 + c * y0 + tx;
+			const y3 = b * x1 + d * y0 + ty;
+			const x4 = a * x1 + c * y1 + tx;
+			const y4 = b * x1 + d * y1 + ty;
+			const x5 = a * x0 + c * y1 + tx;
+			const y5 = b * x0 + d * y1 + ty;
+
+			xmin = Math.min(xmin, x2, x3, x4, x5);
+			xmax = Math.max(xmax, x2, x3, x4, x5);
+			ymin = Math.min(ymin, y2, y3, y4, y5);
+			ymax = Math.max(ymax, y2, y3, y4, y5);
+
+			const x2s = x2.toFixed(precision);
+			const y2s = y2.toFixed(precision);
+			const x3s = x3.toFixed(precision);
+			const y3s = y3.toFixed(precision);
+			const x4s = x4.toFixed(precision);
+			const y4s = y4.toFixed(precision);
+			const x5s = x5.toFixed(precision);
+			const y5s = y5.toFixed(precision);
+
+			result += ` M${x2s},${y2s} L${x3s},${y3s} L${x4s},${y4s} L${x5s},${y5s}Z`;
 		}
-		return result;
+		return `M${xmin},${ymin} L${xmax},${ymin} L${xmax},${ymax} L${xmin},${ymax}Z` + result;
 	}
 
 	protected toRects(clipperExRects: UtilHtmlElementClipperExRects, result: number[]): number {
