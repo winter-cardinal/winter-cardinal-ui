c44269ebcdf91e332644097ca37ca61577384d9d
Masashi Kojo <masashi.kojo@toshiba.co.jp>
2022-07-11 14:21:20 +0900

    fix: EShape "Show/Hide" Action issue

--- src/main/typescript/wcardinal/ui/shape/action/e-shape-action-runtime-show-hide-shape.ts
+++ src/main/typescript/wcardinal/ui/shape/action/e-shape-action-runtime-show-hide-shape.ts
@@ -9,44 +9,17 @@ import { EShapeActionEnvironment } from "./e-shape-action-environment";
 import { EShapeActionRuntimeConditional } from "./e-shape-action-runtime-conditional";
 import { EShapeActionValueShowHideShape } from "./e-shape-action-value-show-hide-shape";
 
-export interface EShapeActionRuntimeShowHideShapeData {
-	condition: boolean | null;
-}
-
 export class EShapeActionRuntimeShowHideShape extends EShapeActionRuntimeConditional {
-	protected _data: Map<EShape, EShapeActionRuntimeShowHideShapeData>;
-
 	constructor(value: EShapeActionValueShowHideShape) {
 		super(value, EShapeRuntimeReset.VISIBILITY);
-		this._data = new Map<EShape, EShapeActionRuntimeShowHideShapeData>();
 	}
 
 	initialize(shape: EShape, runtime: EShapeRuntime): void {
 		super.initialize(shape, runtime);
-		let data = this._data.get(shape);
-		if (data == null) {
-			data = this.newData(shape);
-			if (data != null) {
-				this._data.set(shape, data);
-			}
-		}
 	}
 
 	execute(shape: EShape, runtime: EShapeRuntime, time: number): void {
-		const data = this._data.get(shape);
-		if (data) {
-			const newCondition = this.condition(shape, time, EShapeActionEnvironment);
-			if (data.condition !== newCondition) {
-				data.condition = newCondition;
-				shape.visible = newCondition;
-				runtime.written |= this.reset;
-			}
-		}
-	}
-
-	protected newData(shape: EShape): EShapeActionRuntimeShowHideShapeData | undefined {
-		return {
-			condition: null
-		};
+		shape.visible = this.condition(shape, time, EShapeActionEnvironment);
+		runtime.written |= this.reset;
 	}
 }
