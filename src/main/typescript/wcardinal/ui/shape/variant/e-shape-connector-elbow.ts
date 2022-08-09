817d3d5d48890c5fb800580169b902d1794a96be
Masashi Kojo <masashi.kojo@toshiba.co.jp>
2022-08-09 21:57:38 +0900

    fix: EShapeConnectorElbow point filler

--- src/main/typescript/wcardinal/ui/shape/variant/e-shape-connector-elbow.ts
+++ src/main/typescript/wcardinal/ui/shape/variant/e-shape-connector-elbow.ts
@@ -58,30 +58,38 @@ export class EShapeConnectorElbow extends EShapeConnectorLine {
 		const sxh = 0.5 * EShapeDefaults.SIZE_X;
 		const syh = 0.5 * EShapeDefaults.SIZE_Y;
 
-		// Body
-		const filler = new EShapeConnectorElbowPointsFillter(x0, y0, values);
-		if (0 < bodyValuesLength) {
-			const a = Math.atan2(dy, dx);
-			const c = Math.cos(a);
-			const s = Math.sin(a);
-			const l = Math.sqrt(dx * dx + dy * dy);
-			const x3 = bodyValues[0] * l;
-			const y3 = bodyValues[1];
-			const x4 = cx + c * x3 - s * y3;
-			const y4 = cy + c * y3 + s * x3;
-			filler.tail(x4, y4, tailNormalX, tailNormalY, sxh, syh, tailMargin);
-			for (let i = 2; i < bodyValuesLength; i += 2) {
-				const x = bodyValues[i + 0] * l;
-				const y = bodyValues[i + 1];
-				const x5 = cx + c * x - s * y;
-				const y5 = cy + c * y + s * x;
-				filler.middle(x5, y5);
-			}
-			filler.head(x1, y1, headNormalX, headNormalY, sxh, syh, headMargin);
+		const threshold = 0.000001;
+		if (Math.abs(dx) < threshold && Math.abs(dy) < threshold) {
+			values[0] = x0;
+			values[1] = y0;
+			values[2] = x1;
+			values[3] = y1;
+			values.length = 4;
 		} else {
-			filler.tail(cx, cy, tailNormalX, tailNormalY, sxh, syh, tailMargin);
-			filler.head(x1, y1, headNormalX, headNormalY, sxh, syh, headMargin);
+			const filler = new EShapeConnectorElbowPointsFillter(x0, y0, values);
+			if (0 < bodyValuesLength) {
+				const a = Math.atan2(dy, dx);
+				const c = Math.cos(a);
+				const s = Math.sin(a);
+				const l = Math.sqrt(dx * dx + dy * dy);
+				const x3 = bodyValues[0] * l;
+				const y3 = bodyValues[1];
+				const x4 = cx + c * x3 - s * y3;
+				const y4 = cy + c * y3 + s * x3;
+				filler.tail(x4, y4, tailNormalX, tailNormalY, sxh, syh, tailMargin);
+				for (let i = 2; i < bodyValuesLength; i += 2) {
+					const x = bodyValues[i + 0];
+					const y = bodyValues[i + 1];
+					const x5 = cx + (c * x - s * y) * l;
+					const y5 = cy + (c * y + s * x) * l;
+					filler.middle(x5, y5);
+				}
+				filler.head(x1, y1, headNormalX, headNormalY, sxh, syh, headMargin);
+			} else {
+				filler.tail(cx, cy, tailNormalX, tailNormalY, sxh, syh, tailMargin);
+				filler.head(x1, y1, headNormalX, headNormalY, sxh, syh, headMargin);
+			}
+			filler.margin(tailMargin, headMargin);
 		}
-		filler.margin(tailMargin, headMargin);
 	}
 }
