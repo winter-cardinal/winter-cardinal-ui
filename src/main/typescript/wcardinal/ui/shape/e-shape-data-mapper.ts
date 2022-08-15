500090e8880f7094e98957f225c9962d9409ea84
Masashi Kojo <masashi.kojo@toshiba.co.jp>
2022-08-15 18:49:11 +0900

    fix: the return type of EShapeDataMapper#map(...)

--- src/main/typescript/wcardinal/ui/shape/e-shape-data-mapper.ts
+++ src/main/typescript/wcardinal/ui/shape/e-shape-data-mapper.ts
@@ -9,5 +9,5 @@ import { EShapeDataValue } from "./e-shape-data-value";
  * An EShape data mapper.
  */
 export interface EShapeDataMapper {
-	map(target: EShapeDataValue, destination: string[] | null, initial: string): void;
+	map(target: EShapeDataValue, destination: string[] | null, initial: string): boolean;
 }
