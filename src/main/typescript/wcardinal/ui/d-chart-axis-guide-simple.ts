dc3323cd9990a0cc68a9d94ab0c3f45b7a430d6b
Masashi Kojo <masashi.kojo@toshiba.co.jp>
2024-04-26 13:06:18 +0900

    feat: add fixed DChartAxisGuideSimple

--- src/main/typescript/wcardinal/ui/d-chart-axis-guide-simple.ts
+++ src/main/typescript/wcardinal/ui/d-chart-axis-guide-simple.ts
@@ -9,26 +9,38 @@ import {
 	DChartAxisGuideSimpleShapeOptions
 } from "./d-chart-axis-guide-simple-shape";
 import { DChartAxisGuideSimpleShapeImpl } from "./d-chart-axis-guide-simple-shape-impl";
+import { DChartPlotArea, DChartPlotAreaLike } from "./d-chart-plot-area";
+import { DChartRegionImpl } from "./d-chart-region-impl";
+import { DChartRegion } from "./d-chart-region";
 
 export interface DChartAxisGuideSimpleOptions extends DChartAxisGuideSimpleShapeOptions {
 	position?: number;
+	fixed?: boolean;
 }
 
 export class DChartAxisGuideSimple<CHART extends DBase = DBase> implements DChartAxisGuide<CHART> {
+	protected static WORK_POINT?: Point;
+	protected static WORK_REGION?: DChartRegion;
+
 	protected _axis?: DChartAxis<CHART>;
 	protected _options?: DChartAxisGuideSimpleOptions;
 	protected _container?: DChartAxisContainer<CHART>;
 	protected _index: number;
 	protected _shape?: DChartAxisGuideSimpleShape;
 	protected _position: number;
-	protected _work: Point;
+	protected _fixed: boolean;
 	protected _isShown: boolean;
 
 	constructor(options?: DChartAxisGuideSimpleOptions) {
 		this._options = options;
 		this._index = 0;
-		this._position = options?.position ?? 0;
-		this._work = new Point();
+		if (options != null) {
+			this._position = options.position ?? 0;
+			this._fixed = options.fixed ?? false;
+		} else {
+			this._position = 0;
+			this._fixed = false;
+		}
 		this._isShown = true;
 	}
 
@@ -100,71 +112,82 @@ export class DChartAxisGuideSimple<CHART extends DBase = DBase> implements DChar
 			const plotAreaY = plotAreaBounds.y;
 			const plotAreaWidth = plotAreaBounds.width;
 			const plotAreaHeight = plotAreaBounds.height;
-			const transform = plotArea.container.transform.localTransform;
 			const offset = axis.padding * index;
 			switch (axis.position) {
 				case DChartAxisPosition.TOP:
 				case DChartAxisPosition.BOTTOM:
-					const coordinateX = plotArea.coordinate.x.get(axis.coordinate);
-					if (coordinateX) {
-						const work = this._work;
-						work.set(coordinateX.transform.map(coordinateX.map(this._position)), 0);
-						transform.apply(work, work);
-						const x = work.x;
-						shape.update(
-							axis.position,
-							x,
-							plotAreaY + plotAreaHeight * 0.5,
-							this._isShown && plotAreaX <= x && x <= plotAreaX + plotAreaWidth,
-							plotAreaWidth,
-							plotAreaHeight,
-							offset
-						);
-					} else {
-						shape.update(
-							axis.position,
-							plotAreaX,
-							plotAreaY + plotAreaHeight * 0.5,
-							false,
-							plotAreaWidth,
-							plotAreaHeight,
-							offset
-						);
-					}
+					const x = this.getPositionX(axis, plotArea, plotAreaX);
+					shape.update(
+						axis.position,
+						x,
+						plotAreaY + plotAreaHeight * 0.5,
+						this._isShown && plotAreaX <= x && x <= plotAreaX + plotAreaWidth,
+						plotAreaWidth,
+						plotAreaHeight,
+						offset
+					);
 					break;
 				case DChartAxisPosition.LEFT:
 				case DChartAxisPosition.RIGHT:
-					const coordinateY = plotArea.coordinate.y.get(axis.coordinate);
-					if (coordinateY) {
-						const work = this._work;
-						work.set(0, coordinateY.transform.map(coordinateY.map(this._position)));
-						transform.apply(work, work);
-						const y = work.y;
-						shape.update(
-							axis.position,
-							plotAreaX + plotAreaWidth * 0.5,
-							y,
-							this._isShown && plotAreaY <= y && y <= plotAreaY + plotAreaHeight,
-							plotAreaWidth,
-							plotAreaHeight,
-							offset
-						);
-					} else {
-						shape.update(
-							axis.position,
-							plotAreaX + plotAreaWidth * 0.5,
-							plotAreaY,
-							false,
-							plotAreaWidth,
-							plotAreaHeight,
-							offset
-						);
-					}
+					const y = this.getPositionY(axis, plotArea, plotAreaY);
+					shape.update(
+						axis.position,
+						plotAreaX + plotAreaWidth * 0.5,
+						y,
+						this._isShown && plotAreaY <= y && y <= plotAreaY + plotAreaHeight,
+						plotAreaWidth,
+						plotAreaHeight,
+						offset
+					);
 					break;
 			}
 			return true;
 		}
 		return false;
+	}
+
+	protected getPositionX(
+		axis: DChartAxis<CHART>,
+		plotArea: DChartPlotArea<CHART> | DChartPlotAreaLike<CHART>,
+		plotAreaX: number
+	): number {
+		if (this._fixed) {
+			const work = (DChartAxisGuideSimple.WORK_REGION ??= new DChartRegionImpl(0, 0));
+			plotArea.getPixelDomain(work);
+			return work.from + this._position * (work.to - work.from);
+		} else {
+			const coordinateX = plotArea.coordinate.x.get(axis.coordinate);
+			if (coordinateX) {
+				const work = (DChartAxisGuideSimple.WORK_POINT ??= new Point());
+				work.set(coordinateX.transform.map(coordinateX.map(this._position)), 0);
+				plotArea.container.transform.localTransform.apply(work, work);
+				return work.x;
+			} else {
+				return plotAreaX - 100;
+			}
+		}
+	}
+
+	protected getPositionY(
+		axis: DChartAxis<CHART>,
+		plotArea: DChartPlotArea<CHART> | DChartPlotAreaLike<CHART>,
+		plotAreaY: number
+	): number {
+		if (this._fixed) {
+			const work = (DChartAxisGuideSimple.WORK_REGION ??= new DChartRegionImpl(0, 0));
+			plotArea.getPixelRange(work);
+			return work.to + (1 - this._position) * (work.from - work.to);
+		} else {
+			const coordinateY = plotArea.coordinate.y.get(axis.coordinate);
+			if (coordinateY) {
+				const work = (DChartAxisGuideSimple.WORK_POINT ??= new Point());
+				work.set(0, coordinateY.transform.map(coordinateY.map(this._position)));
+				plotArea.container.transform.localTransform.apply(work, work);
+				return work.y;
+			} else {
+				return plotAreaY - 100;
+			}
+		}
 	}
 
 	destroy(): void {
