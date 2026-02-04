1cc239b4916d5b1dba18782b3c33a774dc8dcf24
Masashi Kojo <masashi.kojo@toshiba.co.jp>
2026-02-04 13:48:36 +0900

    revise: EShapePolygon capability

--- src/main/typescript/wcardinal/ui/shape/load/load-shape-polygon.ts
+++ src/main/typescript/wcardinal/ui/shape/load/load-shape-polygon.ts
@@ -3,6 +3,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import { EShapeCapabilities } from "../e-shape-capabilities";
+import { EShapeCapability } from "../e-shape-capability";
 import { EShapeDeserializers } from "../e-shape-deserializers";
 import { EShapeType } from "../e-shape-type";
 import { EShapeUploadeds } from "../e-shape-uploadeds";
@@ -12,4 +14,8 @@ import { deserializePolygon } from "../variant/deserialize-polygon";
 export const loadShapePolygon = (): void => {
 	EShapeUploadeds[EShapeType.POLYGON] = createPolygonUploaded;
 	EShapeDeserializers[EShapeType.POLYGON] = deserializePolygon;
+	EShapeCapabilities.set(
+		EShapeType.POLYGON,
+		EShapeCapability.PRIMITIVE & ~(EShapeCapability.LINE_HEAD | EShapeCapability.LINE_TAIL)
+	);
 };
