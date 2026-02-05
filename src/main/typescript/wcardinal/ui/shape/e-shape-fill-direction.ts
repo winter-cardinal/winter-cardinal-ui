d7f16f34026e4a898fefd93474b3a0a9f4c106ce
Masashi Kojo <masashi.kojo@toshiba.co.jp>
2026-02-06 00:27:03 +0900

    chore: add comments on EShapeFillDirection

--- src/main/typescript/wcardinal/ui/shape/e-shape-fill-direction.ts
+++ src/main/typescript/wcardinal/ui/shape/e-shape-fill-direction.ts
@@ -3,10 +3,17 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+/**
+ * Represents the fill direction of a shape.
+ */
 export const EShapeFillDirection = {
+	/** Fill from top to bottom */
 	TOP: 0,
+	/** Fill from right to left */
 	RIGHT: 1,
+	/** Fill from bottom to top */
 	BOTTOM: 2,
+	/** Fill from left to right */
 	LEFT: 3
 } as const;
 
