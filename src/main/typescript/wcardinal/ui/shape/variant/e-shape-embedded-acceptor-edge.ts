8e29de06eac90f16a0a7d6f638b832bd8267ca97
Masashi Kojo <masashi.kojo@toshiba.co.jp>
2026-08-21 14:08:46 +0900

    fix: EShapeEmbeddedAcceptorEdge and EShapeLineOfCircles

--- src/main/typescript/wcardinal/ui/shape/variant/e-shape-embedded-acceptor-edge.ts
+++ src/main/typescript/wcardinal/ui/shape/variant/e-shape-embedded-acceptor-edge.ts
@@ -5,9 +5,9 @@ import { EShapeAcceptorEdgeType } from "../e-shape-acceptor-edge-type";
 import { EShapeCopyPart } from "../e-shape-copy-part";
 import { EShapeResourceManagerSerialization } from "../e-shape-resource-manager-serialization";
 import { EShapeType } from "../e-shape-type";
-import { EShapeCircle } from "./e-shape-circle";
+import { EShapeCircleLegacy } from "./e-shape-circle-legacy";
 
-export class EShapeEmbeddedAcceptorEdge extends EShapeCircle {
+export class EShapeEmbeddedAcceptorEdge extends EShapeCircleLegacy {
 	protected _subtype: EShapeAcceptorEdgeType;
 	protected _side: EShapeAcceptorEdgeSide;
 
